3e8936174f32ba9465800ee0dba563e15acf537b
Sam Miodownik <sammiodownik11@gmail.com>
2026-06-04 23:09:49 -0400

    finished numpy practice

--- python-practice/numpy_test.py
+++ python-practice/numpy_test.py
@@ -6,9 +6,35 @@ def Q1():
     c = np.intersect1d(a, b)
     print(c)
 
+def Q2():
+    return np.arange(1, 16).reshape(3, 5).T
+
+def Q3():
+    a = Q2()
+    print(a.T.flatten())
+
+def Q4():
+    a = Q2()
+    a= a.T.reshape(3,5,1)
+    return a
+
+def Q5():
+    a = Q4()
+    a.reshape(5, 3)
+    print(a)
+
+def Q6():
+    a = np.array([12, 5, 7, 15, 3, 1, 8])
+    b = np.array([14, 6, 3, 11, 19, 12, 5])
+    return np.setdiff1d(a, b)
+
 def main():
     Q1()
-
+    print(Q2())
+    Q3()
+    print(Q4())
+    Q5()
+    print(Q6())
 
 if __name__ == "__main__":
     main()
